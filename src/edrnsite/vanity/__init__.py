de2c098511b0991d6e92115bda5aafc1ffdfe34f
Sean Kelly <sean.c.kelly@me.com>
2013-01-23 03:04:03 +0000

    constants

--- src/edrnsite/vanity/__init__.py
+++ src/edrnsite/vanity/__init__.py
@@ -5,6 +5,16 @@
 '''EDRN Site: vanity pages.
 '''
 
+from datetime import timedelta
 from zope.i18nmessageid import MessageFactory
 
-MESSAGE_FACTORY = MessageFactory('edrnsite.vanity')
+# Session update key, and possible values
+VANITY_UPDATE_KEY = __name__ + '.vanityUpdate'
+BESPOKE_OLD       = 'bespoke-old'
+BESPOKE_WELCOME   = 'bespoke-welcome'
+
+# How long to wait (in days) before nagging theuser
+NAG_LIMIT         = timedelta(360)
+
+# i18n
+MESSAGE_FACTORY   = MessageFactory('edrnsite.vanity')
