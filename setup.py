6a05b484d89ecb730f06a58bfa37783935301dad
Sean Kelly <sean.c.kelly@me.com>
2013-02-05 17:44:41 +0000

    bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os.path
 # ------------
 
 _name            = 'edrnsite.vanity'
-_version         = '0.0.0'
+_version         = '0.0.1'
 _description     = 'EDRN Site Vanity'
 _url             = 'http://cancer.jpl.nasa.gov/'
 _downloadURL     = 'http://oodt.jpl.nasa.gov/dist/edrnsite'
