9d26a51115002707bc71a575ed4a51d6c022a577
Sean Kelly <sean.c.kelly@me.com>
2013-02-06 02:33:44 +0000

    Add a member-pages folder on installation so members can actually have pages

---
+++ src/edrnsite/vanity/setuphandlers.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+# Copyright 2013 California Institute of Technology. ALL RIGHTS
+# RESERVED. U.S. Government Sponsorship acknowledged.
+
+from Products.CMFCore.utils import getToolByName
+
+def installMemberPagesFolder(portal):
+    if 'member-pages' not in portal.keys():
+        folder = portal[portal.invokeFactory('Folder', 'member-pages')]
+        folder.setTitle(u'Member Pages')
+        folder.setDescription(u'Personal pages of EDRN members.')
+        folder.reindexObject()
+    else:
+        folder = portal['member-pages']
+    wfTool = getToolByName(portal, 'portal_workflow')
+    state = wfTool.getInfoFor(folder, 'review_state')
+    if state != 'published':
+        wfTool.doActionFor(folder, 'publish')
+    localRoles = folder.get_local_roles()
+    found = False
+    for principal, roles in localRoles:
+        if principal == 'AuthenticatedUsers':
+            if u'Contributor' in roles:
+                found = True
+                break
+    if not found:
+        folder.manage_setLocalRoles('AuthenticatedUsers', ['Contributor'])
+
+
+def setupImportSteps(context):
+    if context.readDataFile('edrnsite.vanity.marker.txt') is None: return
+    portal = context.getSite()
+    installMemberPagesFolder(portal)
