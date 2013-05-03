1d991b3a542537141ed8ae453266f80b45452fd9
Sean Kelly <sean.c.kelly@me.com>
2013-05-03 21:52:47 +0000

    Add vanity page enable/disable utiity

--- src/edrnsite/vanity/__init__.py
+++ src/edrnsite/vanity/__init__.py
@@ -7,9 +7,13 @@
 
 from datetime import timedelta
 from zope.i18nmessageid import MessageFactory
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+
+PACKAGE_NAME = __name__
 
 # Session update key, and possible values
-VANITY_UPDATE_KEY = __name__ + '.vanityUpdate'
+VANITY_UPDATE_KEY = PACKAGE_NAME + '.vanityUpdate'
 BESPOKE_OLD       = 'bespoke-old'
 BESPOKE_WELCOME   = 'bespoke-welcome'
 
@@ -17,4 +21,10 @@ BESPOKE_WELCOME   = 'bespoke-welcome'
 NAG_LIMIT         = timedelta(360)
 
 # i18n
-MESSAGE_FACTORY   = MessageFactory('edrnsite.vanity')
+MESSAGE_FACTORY   = MessageFactory(PACKAGE_NAME)
+
+# Utilities
+def vanityPagesEnabled():
+    return getUtility(IRegistry).get('edrnsite.vanity.enable', False)
+
+
